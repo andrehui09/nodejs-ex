85775ddb22973898aeeb29ba3ec72502540a6714
andrehui09 <andrehui09@gmail.com>
2019-01-21 12:45:01 +0000

    asdf

--- server.js
+++ server.js
@@ -90,12 +90,10 @@ app.get('/pagecount', function (req, res) {
 
 // error handling
 app.use(function(err, req, res, next){
-  console.error(err.stack);
   res.status(500).send('Something bad happened!');
 });
 
 initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
 });
 
 
